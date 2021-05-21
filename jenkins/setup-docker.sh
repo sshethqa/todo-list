27ecb198e0d336d22c6a02c91e2b7c7d5a52e8bd
htr-volker <hvolker@qa.com>
2021-05-21 14:13:36 +0000

    jenkinsfile adds jenkins to docker group

--- jenkins/setup-docker.sh
+++ jenkins/setup-docker.sh
@@ -3,5 +3,8 @@
 # Install docker
 curl https://get.docker.com | sudo bash
 
+# Add jenkins to docker group
+sudo usermod -aG docker jenkins
+
 # Log in to Docker 
 docker login -u $DOCKER_LOGIN_USR -p $DOCKER_LOGIN_PSW
